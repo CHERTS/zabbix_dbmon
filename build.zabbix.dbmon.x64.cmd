50b8c9532c7ab14b8d2af7fcb69d926297499a25
Mikhail Grigorev <sleuthhound@gmail.com>
2020-04-01 14:18:12 +0500

    Added Windows build script

---
+++ build.zabbix.dbmon.x64.cmd
@@ -0,0 +1,64 @@
+@echo off
+
+title Build zabbix-agent-dbmon...
+
+set ZBX_ARCH=AMD64
+set VS_ARCH=x64
+set PCRE_PATH=D:\Neo\Project\zabbix\zabbix_dbmon_github\vs2017\pcre
+set OPENSSL_PATH=D:\Neo\Project\zabbix\zabbix_dbmon_github\vs2017\openssl
+set PTHREADS4W_PATH=D:\Neo\Project\zabbix\zabbix_dbmon_github\vs2017\pthreads4w
+set LIBCONIG_PATH=D:\Neo\Project\zabbix\zabbix_dbmon_github\vs2017\libconfig
+set MYSQL_PATH=D:\Neo\Project\zabbix\zabbix_dbmon_github\vs2017\mariadb
+set PGSQL_PATH=D:\Neo\Project\zabbix\zabbix_dbmon_github\vs2017\postgresql_12
+set ORACLE_PATH=D:\Neo\Project\zabbix\zabbix_dbmon_github\vs2017\oracle_18
+
+title Build zabbix-agent-dbmon %ZBX_ARCH%...
+
+rem https://support.zabbix.com/browse/ZBXNEXT-3047
+rem -- Visual Studio 2015 Pro --
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %VS_ARCH% >nul 2>&1
+rem -- Visual Studio 2017 Community --
+echo Set Visual Studio 2017 enviroment...
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %VS_ARCH% >nul 2>&1
+
+if not exist "%PCRE_PATH%\lib\%VS_ARCH%" (
+  echo ERROR: PCRE %VS_ARCH% not found.
+  echo Press Enter to exit.
+  pause >nul
+  exit 1
+)
+
+if not exist "%OPENSSL_PATH%\lib\%VS_ARCH%" (
+  echo ERROR: OpenSSL %VS_ARCH% not found.
+  echo Press Enter to exit.
+  pause >nul
+  exit 1
+)
+
+if not exist "%PTHREADS4W_PATH%\lib\%VS_ARCH%" (
+  echo ERROR: PTHREADS4W %VS_ARCH% not found.
+  echo Press Enter to exit.
+  pause >nul
+  exit 1
+)
+
+if not exist "%LIBCONIG_PATH%\lib\%VS_ARCH%" (
+  echo ERROR: LIBCONIG %VS_ARCH% not found.
+  echo Press Enter to exit.
+  pause >nul
+  exit 1
+)
+
+if exist "build" (
+  cd build\win32\project
+  copy ..\include\config.h ..\..\..\include\ >nul 2>&1
+  echo Build Zabbix Agent DBMON %ZBX_ARCH%...
+  nmake CPU=%ZBX_ARCH% TLS=openssl TLSINCDIR="%OPENSSL_PATH%\include" TLSLIB="%OPENSSL_PATH%\lib\%VS_ARCH%\ssleay32MT.lib" TLSLIB2="%OPENSSL_PATH%\lib\%VS_ARCH%\libeay32MT.lib" PCREINCDIR="%PCRE_PATH%\include" PCRELIBDIR="%PCRE_PATH%\lib\%VS_ARCH%" PTHREADS4WINCDIR="%PTHREADS4W_PATH%\include" PTHREADS4WLIBDIR="%PTHREADS4W_PATH%\lib\%VS_ARCH%" LIBCONIGINCDIR="%LIBCONIG_PATH%\include" LIBCONIGLIBDIR="%LIBCONIG_PATH%\lib\%VS_ARCH%" DBMON=yes DBMON_MYSQL=yes MYSQLINCDIR="%MYSQL_PATH%\include" MYSQLLIBDIR="%MYSQL_PATH%\lib\%VS_ARCH%" /f Makefile_agent
+  rem echo Build Zabbix Sender %ZBX_ARCH%...
+  rem nmake CPU=%ZBX_ARCH% TLS=openssl TLSINCDIR="%OPENSSL_PATH%\include" TLSLIB="%OPENSSL_PATH%\lib\%VS_ARCH%\ssleay32MT.lib" TLSLIB2="%OPENSSL_PATH%\lib\%VS_ARCH%\libeay32MT.lib" PCREINCDIR="%PCRE_PATH%\include" PCRELIBDIR="%PCRE_PATH%\lib\%VS_ARCH%" /f Makefile_sender
+  rem echo Build Zabbix Get %ZBX_ARCH%...
+  rem nmake CPU=%ZBX_ARCH% TLS=openssl TLSINCDIR="%OPENSSL_PATH%\include" TLSLIB="%OPENSSL_PATH%\lib\%VS_ARCH%\ssleay32MT.lib" TLSLIB2="%OPENSSL_PATH%\lib\%VS_ARCH%\libeay32MT.lib" PCREINCDIR="%PCRE_PATH%\include" PCRELIBDIR="%PCRE_PATH%\lib\%VS_ARCH%" /f Makefile_get
+)
+
+echo Done, press Enter to exit.
+pause >nul
