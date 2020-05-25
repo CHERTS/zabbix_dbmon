8068ada4e1e7b3643516c09ac0fc8a17c7457719
Mikhail Grigorev <sleuthhound@gmail.com>
2020-05-25 17:42:47 +0500

    Update windows batch utils

--- bin/win64/copy_new.cmd
+++ bin/win64/copy_new.cmd
@@ -1,36 +1,53 @@
 @echo off
 
-title Update zabbix-agent...
+title Update zabbix-agents...
 
-set service_name="Zabbix Agent [MYHOMEPC]"
-set src_dir="D:\Project\zabbix_dbmon_github\bin\win64"
+set service_name_list="Zabbix Agent [DB01]" "Zabbix Agent [DB02]"
+set zbx_dbmon_root=D:\Neo\Project\zabbix\zabbix_dbmon_github
+set zbx_agent_path=C:\DBS_Zabbix
+
+net session >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+	echo =======================================================================
+	echo.
+	echo Error: To copy new zabbix-ahent binary need admin rights.
+	echo.
+	echo Run the script as an administrator.
+	echo.
+	echo To run as administrator, click on the file the right mouse
+	echo button and select 'Run as administrator'
+	echo.
+	echo =======================================================================
+	echo Press Enter to exit.
+	pause >nul
+	goto :eof
+)
 
 :stop
-echo Stoping service %service_name%...
-sc stop %service_name%>nul
+for %%a in (%service_name_list%) do (
+	echo Stoping service '%%a'...
+	sc stop %%a>nul
+)
 
-rem cause a ~3 second sleep before checking the service state
+rem cause a ~10 second sleep before checking the service state
 echo Wait 3 sec...
-ping 127.0.0.1 -n 3 -w 1000 > nul
+ping 127.0.0.1 -n 5 -w 1000 > nul
 
-sc query %service_name% | find /I "STATE" | find "STOPPED">nul
-if errorlevel 1 goto :stop
+for %%a in (%service_name_list%) do (
+	echo Checking service '%%a'...
+	sc query %%a | find /I "STATE" | find "STOPPED">nul
+	if errorlevel 1 goto :stop
+)
 goto :copy
 
 :copy
 echo Copy new version of zabbix_agentd.exe...
-copy /Y %src_dir%\zabbix_agentd.exe C:\DBS_Zabbix>nul
-if errorlevel 1 goto :error
+copy /Y %zbx_dbmon_root%\bin\win64\zabbix_agentd.exe %zbx_agent_path%>nul
+if errorlevel 1 goto :stop
 goto :start
 
 :start
-echo Starting service %service_name%...
-net start | find /i %service_name%>nul && goto :start
-sc start %service_name%>nul
-goto :end
-
-:error
-echo ERROR: Failed to update zabbix_agentd.exe
-goto :start
-
-:end
+for %%a in (%service_name_list%) do (
+	echo Starting service '%%a'...
+	sc start %%a>nul
+)
--- bin/win64/restart.cmd
+++ bin/win64/restart.cmd
@@ -1,22 +1,45 @@
 @echo off
 
-title Restart zabbix-agent..
+title Restarting all agents...
 
-set service_name="Zabbix Agent [MYHOMEPC]"
+set service_name_list="Zabbix Agent [DB01]" "Zabbix Agent [DB02]"
+
+net session >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+	echo =======================================================================
+	echo.
+	echo Error: To restart service need admin rights.
+	echo.
+	echo Run the script as an administrator.
+	echo.
+	echo To run as administrator, click on the file the right mouse
+	echo button and select 'Run as administrator'
+	echo.
+	echo =======================================================================
+	echo Press Enter to exit.
+	pause >nul
+	goto :eof
+)
 
 :stop
-echo Stoping service...
-sc stop %service_name%>nul
+for %%a in (%service_name_list%) do (
+	echo Stoping service '%%a'...
+	sc stop %%a>nul
+)
 
-rem cause a ~3 second sleep before checking the service state
+rem cause a ~10 second sleep before checking the service state
 echo Wait 3 sec...
-ping 127.0.0.1 -n 3 -w 1000 > nul
+ping 127.0.0.1 -n 5 -w 1000 > nul
 
-sc query %service_name% | find /I "STATE" | find "STOPPED">nul
-if errorlevel 1 goto :stop
+for %%a in (%service_name_list%) do (
+	echo Checking service '%%a'...
+	sc query %%a | find /I "STATE" | find "STOPPED">nul
+	if errorlevel 1 goto :stop
+)
 goto :start
 
 :start
-echo Starting service...
-net start | find /i %service_name%>nul && goto :start
-sc start %service_name%>nul
+for %%a in (%service_name_list%) do (
+	echo Starting service '%%a'...
+	sc start %%a>nul
+)
