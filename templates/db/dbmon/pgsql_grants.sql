720fc3dc935301f9d39d601be284b16d2ecdb460
Mikhail Grigorev <sleuthhound@gmail.com>
2020-05-29 22:45:27 +0500

    Fixed PostgreSQL docs

--- templates/db/dbmon/pgsql_grants.sql
+++ templates/db/dbmon/pgsql_grants.sql
@@ -1,3 +1,9 @@
+Create user zabbixmon
+---------------------
+
+Connect to database postgres from user postgres:
+# psql -U postgres -d postgres -w
+
 For PostgreSQL version 10 and above:
 
 CREATE USER zabbixmon WITH PASSWORD 'zabbixmon' INHERIT;
@@ -6,8 +12,10 @@ GRANT pg_monitor TO zabbixmon;
 For PostgreSQL version 9.6 and below:
 CREATE USER zabbixmon WITH PASSWORD 'zabbixmon';
 GRANT SELECT ON pg_stat_database TO zabbixmon;
--- To collect WAL metrics, the user must have a `superuser` role.
-ALTER USER zabbixmon WITH SUPERUSER;
+
+-- To collect WAL metrics.
+GRANT EXECUTE ON FUNCTION pg_catalog.pg_ls_dir(text) TO zabbixmon;
+GRANT EXECUTE ON FUNCTION pg_catalog.pg_stat_file(text) TO zabbixmon;
 
 Edit pg_hba.conf to allow connections from Zabbix agent (add these lines to the beginning of the file):
 
@@ -35,3 +43,25 @@ Example 2:
 Set file permition and owner:
 chown zabbix:zabbix /var/lib/zabbix/.pgpass
 chmod 600 /var/lib/zabbix/.pgpass
+
+Revoke grants and drop user zabbixmon
+-------------------------------------
+
+# psql -U postgres -d postgres -w
+
+1) Check Ownership:
+postgres=# select * from pg_shdepend where refclassid = 'pg_authid'::regclass and refobjid = (select oid from pg_roles where rolname = 'zabbixmon');
+
+2) Reassigning Ownership:
+postgres=# REASSIGN OWNED BY zabbixmon TO postgres;
+postgres=# DROP OWNED BY zabbixmon;
+
+3) Executing REVOKE PRIVILEGES:
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM zabbixmon;
+REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM zabbixmon;
+REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM zabbixmon;
+
+4) Dropping the user:
+postgres=# DROP USER zabbixmon;
+
+PS: You might not need to execute both Step 2 and 3, just one of the two steps might be usually enough.
