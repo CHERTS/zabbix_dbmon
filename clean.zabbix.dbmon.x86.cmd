19cbb4fa4a1497b2e52e4adbb4b2febb123083ea
Mikhail Grigorev <sleuthhound@gmail.com>
2020-10-22 17:09:36 +0500

    Update build script

---
+++ clean.zabbix.dbmon.x86.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+title Clean zabbix-agent-dbmon x86...
+
+if exist "build" (
+  echo ------------ Clean DBMON code ------------
+  del /s /q /f *.o
+  del /s /q /f bin\*.pdb
+  del /s /q /f bin\*.exe.pdb
+  del /s /q /f build\*.pdb
+  del /s /q /f build\*.exe.pdb
+  del /s /q /f build\*.exe.idb
+  del /s /q /f bin\win32\*.exe
+  del /s /q /f build\win32\include\messages.h
+  del /s /q /f build\win32\include\messages.rc
+  del /s /q /f build\win32\include\MSG00001.bin
+  del /s /q /f build\win32\project\messages.h
+  del /s /q /f build\win32\project\messages.rc
+  del /s /q /f build\win32\project\MSG00001.bin
+  del /s /q /f build\win32\project\*.res
+)
+
+echo Done
